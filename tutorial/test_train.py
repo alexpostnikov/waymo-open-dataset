5ae786008d09b7be660258607dc79b98eb542297
Alex Postnikov <alex.postnikov@skolkovotech.ru>
2022-03-26 10:30:50 +0300

    FIX import issues

--- tutorial/test_train.py
+++ tutorial/test_train.py
@@ -7,9 +7,10 @@
 
 import tensorflow as tf
 
-from tutorial.test.models import Model
-from tutorial.test.train import train
-from tutorial.test.visualize import vis_cur_and_fut
+from test.models import Model
+from test.train import train
+from test.visualize import vis_cur_and_fut
+from test.train import get_speed_ade_with_mask, get_ade_from_pred_speed_with_mask
 
 tf.get_logger().setLevel('ERROR')
 
@@ -202,90 +203,6 @@ class CustomImageDataset(torch.utils.data.IterableDataset):
             self.iterator = iter(self.dataset)
             return
         raise StopIteration
-
-
-def get_future(data):
-    bs = data["state/future/x"].shape[0]
-    gt_fut = torch.cat([data["state/future/x"].reshape(bs, 128, 80, 1), data["state/future/y"].reshape(bs, 128, 80, 1)],
-                       -1)
-    gt_fut = gt_fut.permute(0, 2, 1, 3)
-    # bs, 80, 128, 2
-    return gt_fut
-
-
-def get_current(data):
-    cur = torch.cat([data["state/current/x"].reshape(-1, 1, 128, 1), data["state/current/y"].reshape(-1, 1, 128, 1)],
-                    -1)
-    return cur
-
-
-def get_future_speed(data, num_ped=128, future_steps=80):
-    bs = data["state/future/x"].shape[0]
-    gt_fut = get_future(data)
-    assert gt_fut.shape == torch.Size([bs, future_steps, num_ped, 2])
-    cur = get_current(data)
-    assert cur.shape == torch.Size([bs, 1, num_ped, 2])
-    gt_fut[:, 1:, :, :] = gt_fut[:, 1:, :, :] - gt_fut[:, :-1, :, :]
-    gt_fut[:, 0:1] = gt_fut[:, 0:1] - cur
-    return gt_fut
-
-
-def get_valid_data_mask(data):
-    bs = data["state/future/x"].shape[0]
-    fut_valid = data["state/future/valid"].reshape(bs, 128, -1) * (data["state/current/valid"].reshape(bs, 128, -1) > 0)
-    fut_valid *= (data["state/past/valid"].reshape(bs, 128, 10).sum(2) == 10).reshape(bs, 128, 1) > 0
-    return fut_valid
-
-
-def pred_to_future(data, pred, num_ped=128, future_steps=80):
-    bs = data["state/future/x"].shape[0]
-    pred_poses = pred.clone()
-    cur = get_current(data).reshape(-1, 128, 2)
-    assert pred.shape == torch.Size([bs, num_ped, future_steps, 2])
-    pred_poses[:, :, 0] += cur.to(pred.device)
-    pred_poses = torch.cumsum(pred_poses, 2)
-    return pred_poses
-
-
-def get_speed_ade_with_mask(data, pred, num_ped=128, future_steps=80):
-    bs = data["state/future/x"].shape[0]
-    assert pred.shape == torch.Size([bs, num_ped, future_steps, 2])
-    gt_fut = get_future(data)
-    assert gt_fut.shape == torch.Size([bs, future_steps, num_ped, 2])
-
-    cur = get_current(data)
-    assert cur.shape == torch.Size([bs, 1, num_ped, 2])
-    gt_fut_speed = get_future_speed(data)
-    dist = torch.norm(pred.permute(0, 2, 1, 3) - gt_fut_speed.cuda(), dim=3)
-    valid = get_valid_data_mask(data)
-    mask = data["state/tracks_to_predict"].reshape(-1, 128, 1).repeat(1, 1, 80) * valid
-    mask = mask.permute(0, 2, 1)
-    dist_masked = dist[mask > 0]
-    #     dist = dist[dist<500]
-    return dist_masked
-
-
-def get_ade_from_pred_speed_with_mask(data, pred, num_ped=128, future_steps=80):
-    bs = data["state/future/x"].shape[0]
-    assert pred.shape == torch.Size([bs, num_ped, future_steps, 2])
-    cur = get_current(data)
-    assert cur.shape == torch.Size([bs, 1, num_ped, 2])
-    loss = pred + 0
-    loss[:, :, 0] = pred[:, :, 0] + cur[:, 0].to(pred.device)
-    loss = torch.cumsum(loss, dim=2)
-    gt_fut = get_future(data).to(pred.device)
-    assert gt_fut.shape == torch.Size([bs, future_steps, num_ped, 2])
-    dist = torch.norm((loss.permute(0, 2, 1, 3) - gt_fut), dim=3)
-    valid = get_valid_data_mask(data)
-    mask = data["state/tracks_to_predict"].reshape(-1, 128, 1).repeat(1, 1, 80) * valid
-    mask = mask.permute(0, 2, 1)
-    dist_masked = dist[mask > 0]
-    return dist_masked
-
-
-def ade_loss(data, pred):
-    ade = get_speed_ade_with_mask(data, pred)
-    return ade.mean()
 
 
 batch_size = 8
