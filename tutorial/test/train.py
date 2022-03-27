bd08d9332b4cfefac9b24e8c034a4f11b3af1661
Alex Postnikov <alex.postnikov@skolkovotech.ru>
2022-03-27 23:14:02 +0300

    multimodal loss

--- tutorial/test/train.py
+++ tutorial/test/train.py
@@ -1,5 +1,8 @@
 import torch
 from tqdm.auto import tqdm
+import torch.nn as nn
+import numpy as np
+from einops import rearrange
 
 # from test_train import get_ade_from_pred_speed_with_mask, get_speed_ade_with_mask
 
@@ -28,6 +31,51 @@ def train(model, loader, optimizer, num_ep=10):
                 if len(losses) > 500:
                     losses = losses[100:]
 
+
+def train_multymodal(model, loader, optimizer, num_ep=10, checkpointer=None, logger=None):
+    losses = torch.rand(0)
+    for epoch in range(num_ep):  # loop over the dataset multiple times
+        pbar = tqdm(loader)
+        for chank, data in enumerate(pbar):
+            optimizer.zero_grad()
+            poses, confs = model(data)
+            # poses -> bs, num_peds, times, modes, 2
+            loss = pytorch_neg_multi_log_likelihood_batch(data, poses, confs).mean()
+            loss.backward()
+
+            optimizer.step()
+            with torch.no_grad():
+                # min ade:
+                uni_ades = []
+                for mode in range(poses.shape[3]):
+                        ades = uni_ade_poses(data, poses[:,:,:,mode]) # calc uni ade. 1.pred = pred+cur, 2. torch.norm(pred, gt), 3. apply mask
+                        uni_ades.append(ades.detach())
+                m_ades = torch.stack(uni_ades, dim=1)
+                m_ades = torch.min(m_ades, dim=1).values
+
+                valid = get_valid_data_mask(data)
+                mask = data["state/tracks_to_predict"].reshape(-1, 128, 1).repeat(1, 1, 80) * valid
+                m_ade = m_ades[mask>0].mean()
+                losses = torch.cat([losses, torch.tensor([loss.detach().item()])], 0)
+                pbar.set_description("ep %s chank %s" % (epoch, chank))
+                pbar.set_postfix({"loss": losses.mean().item(), "m_ade": m_ade.item()})
+                logger.log({"loss": loss,
+                            "min_ade": m_ade.item()})
+                if len(losses) > 500:
+                    losses = losses[100:]
+        checkpointer.save(epoch, losses.mean().item())
+
+
+def uni_ade_poses(data, predictions):
+    bs, num_ped, future_steps, _ = predictions.shape
+    gt_fut = get_future(data).to(predictions.device)
+    assert gt_fut.shape == torch.Size([bs, future_steps, num_ped, 2])
+
+    cur = get_current(data).to(predictions.device)
+    assert cur.shape == torch.Size([bs, 1, num_ped, 2])
+    pred_moved = predictions + cur.permute(0,2,1,3)
+    dist = torch.norm((pred_moved - gt_fut.permute(0,2,1,3)), dim=-1)
+    return dist
 
 def get_future(data):
     bs = data["state/future/x"].shape[0]
@@ -111,3 +159,51 @@ def get_ade_from_pred_speed_with_mask(data, pred, num_ped=128, future_steps=80):
 def ade_loss(data, pred):
     ade = get_speed_ade_with_mask(data, pred)
     return ade.mean()
+
+
+def pytorch_neg_multi_log_likelihood_batch(data, logits, confidences):
+    """
+    Compute a negative log-likelihood for the multi-modal scenario.
+    Args:
+        gt (Tensor): array of shape (bs)x(time)x(2D coords)
+        logits (Tensor): array of shape (bs)x(modes)x(time)x(2D coords)
+        confidences (Tensor): array of shape (bs)x(modes) with a confidence for each mode in each sample
+        avails (Tensor): array of shape (bs)x(time) with the availability for each gt timestep
+    Returns:
+        Tensor: negative log-likelihood for this example, a single float number
+    """
+
+    bs, num_ped = logits.shape[:2]
+    # convert to (batch_size, num_modes, future_len, num_coords)
+    gt_fut = get_future(data).to(logits.device)
+    gt = rearrange(gt_fut, "bs timestemps num_peds data_dim -> (bs num_peds) timestemps data_dim")
+    gt = torch.unsqueeze(gt, 1)  # add modes
+    valid = get_valid_data_mask(data)
+    mask = data["state/tracks_to_predict"].reshape(-1, 128, 1).repeat(1, 1, 80) * valid
+    avails = mask.permute(0, 2, 1)
+    avails = rearrange(avails, "bs timestemps num_peds -> (bs num_peds) timestemps")
+    avails = avails[:, None, :, None].to(logits.device)  # add modes and cords
+
+    logits = rearrange(logits, "bs num_peds timestemps modes data_dim -> (bs num_peds) modes timestemps   data_dim")
+    cur = get_current(data)
+    assert cur.shape == torch.Size([bs, 1, num_ped, 2])
+    cur = rearrange(cur, "bs time num_peds data -> (bs num_peds) 1 time data").to(logits.device)
+    logits_moved = logits+cur
+    confidences = rearrange(confidences, "bs num_peds modes -> (bs num_peds) modes")
+    # error (batch_size, num_modes, future_len)
+    error = torch.sum(
+        ((gt - logits_moved) * avails) ** 2, dim=-1
+    )  # reduce coords and use availability
+
+    with np.errstate(
+        divide="ignore"
+    ):  # when confidence is 0 log goes to -inf, but we're fine with it
+        # error (batch_size, num_modes)
+        error = nn.functional.log_softmax(confidences, dim=1) - 0.5 * torch.sum(
+            error, dim=-1
+        )  # reduce time
+
+    # error (batch_size, num_modes)
+    error = -torch.logsumexp(error, dim=-1, keepdim=True)
+
+    return torch.mean(error)
