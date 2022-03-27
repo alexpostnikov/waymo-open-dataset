411d928a79ac34242c86264671980cf50055695d
Alex Postnikov <alex.postnikov@skolkovotech.ru>
2022-03-27 23:13:42 +0300

    saver

--- tutorial/test/models.py
+++ tutorial/test/models.py
@@ -160,3 +160,62 @@ class SimplModel(nn.Module):
         state = (torch.cat([cur, past], 1) - cur).permute(0, 2, 1, 3).reshape(-1, 128, 11 * 2).cuda()
         out = self.dec(state).reshape(-1, 128, 80, 2)
         return out
+
+
+class Checkpointer:
+  """A simple `PyTorch` model load/save wrapper."""
+  def __init__(
+      self,
+      model: nn.Module,
+      torch_seed: int,
+      ckpt_dir: str,
+      checkpoint_frequency: int
+  ) -> None:
+    """Constructs a simple load/save checkpointer."""
+    self._model = model
+    self._torch_seed = torch_seed
+    self._ckpt_dir = ckpt_dir
+    self._best_validation_loss = float('inf')
+
+    # The number of times validation loss must improve prior to our
+    # checkpointing of the model
+    if checkpoint_frequency == -1:
+      self.checkpoint_frequency = float('inf')  # We will never checkpoint
+    else:
+      self.checkpoint_frequency = checkpoint_frequency
+
+    self._num_improvements_since_last_ckpt = 0
+
+    os.makedirs(self._ckpt_dir, exist_ok=True)
+
+  def save(
+      self,
+      epoch: int,
+      new_validation_loss: float
+  ) -> str:
+    """Saves the model to the `ckpt_dir/epoch/model.pt` file."""
+    model_file_name = f'model-seed-{self._torch_seed}-epoch-{epoch}.pt'
+
+    if new_validation_loss < self._best_validation_loss:
+      self._num_improvements_since_last_ckpt += 1
+      self._best_validation_loss = new_validation_loss
+
+      if self._num_improvements_since_last_ckpt >= self.checkpoint_frequency:
+        print(
+          f'Validation loss has improved '
+          f'{self._num_improvements_since_last_ckpt} times since last ckpt, '
+          f'storing checkpoint {model_file_name}.')
+    ckpt_path = os.path.join(self._ckpt_dir, model_file_name)
+    torch.save(self._model.state_dict(), ckpt_path)
+    self._num_improvements_since_last_ckpt = 0
+    return ckpt_path
+
+  def load(
+      self,
+      epoch: int,
+  ) -> nn.Module:
+    """Loads the model from the `ckpt_dir/epoch/model.pt` file."""
+    model_file_name = f'model-seed-{self._torch_seed}-epoch-{epoch}.pt'
+    ckpt_path = os.path.join(self._ckpt_dir, model_file_name)
+    self._model.load_state_dict(torch.load(ckpt_path, map_location=device))
+    return self._model
